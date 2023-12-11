a1272a3ccd0af250e2c749b02dd364283fae04b7
VictoriaTorry <k.victoria.torry@gmail.com>
2023-12-11 13:23:34 +0200

    divided ContactList and ContactItem

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,23 +1,11 @@
+import { ContactItem } from 'components/ContactItem/ContactItem';
 import css from './ContactList.module.css';
 
 export const ContactList = ({ contacts, onDelete }) => {
   return (
     <ul className={css.contactList}>
-      {contacts.map(({ id, name, number }) => {
-        return (
-          <li key={id} className={css.contactItem}>
-            <p className={css.contactName}>
-              {name}: {number}
-            </p>
-            <button
-              onClick={() => onDelete(id)}
-              type="button"
-              className={css.contactBtn}
-            >
-              Delete
-            </button>
-          </li>
-        );
+      {contacts.map(contact => {
+        return <ContactItem contact={contact} onDelete={onDelete} />;
       })}
     </ul>
   );
